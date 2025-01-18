f194efcd4042a2d0ac6b16de5a0710e4344e01d4
ojas kaushik <ojaskaushik99@gmail.com>
2025-01-18 15:06:17 +0530

    Second program Added

---
+++ number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class number{
+   public static void main(String[] args) {
+      String [] arr = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      System.out.println(arr[num]);
+}
+}
+
